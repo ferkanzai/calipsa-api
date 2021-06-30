9120e4886ab0e6d370e218ac7d5a098863ce99ba
Fernando Carmona <fernando.carayu@gmail.com>
2021-06-30 22:07:14 +0200

    GET /alarms paginated

---
+++ src/routes/alarms/alarms.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+
+const alarms = require("../../../data.1625071215.json").alarms;
+
+router.get("/", (req, res, next) => {
+  let { page = 1, limit = 25 } = req.query;
+
+  page = Number(page);
+  limit = Number(limit);
+
+  const offset = page * limit - limit;
+
+  const alarmsPaginated = alarms.slice(offset, limit * page);
+
+  try {
+    res.status(200).json({
+      success: true,
+      nextPage: alarmsPaginated.length < limit ? null : page + 1,
+      count: alarmsPaginated.length,
+      data: alarmsPaginated,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 
 router.use("/ping", require("./ping"));
 router.use("/locations", require("./locations"));
+router.use("/alarms", require("./alarms"));
 
 module.exports = router;
